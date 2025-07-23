220d09eeb918dc7f34d5b0d8eca4f77c7ce8f164
Boy Sitorus <sitorusboy0123@gmail.com>
2025-07-23 19:15:14 +0700

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,25 @@
+import { ThemeToggle } from "../components/ThemeToggle";
+import { StarBackground } from "../components/StarBackground";
+import { Navbar } from "../components/NavBar";
+import { HeroSection } from "../components/HeroSection";
+import { AboutSection } from "../components/AboutSection";
+import { SkillSection } from "../components/SkillSection";
+import { ProjectSection } from "../components/ProjectSection";
+import { ContactSection } from "../components/ContactSection";
+
+export const Home = () => {
+    return (
+        <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+            <ThemeToggle />
+            <StarBackground />
+            <Navbar /> 
+            <main>
+                <HeroSection />
+                <AboutSection />
+                <SkillSection />
+                <ProjectSection />
+                <ContactSection />
+            </main>
+        </div>
+    );
+}
